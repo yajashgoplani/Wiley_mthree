e39e36abb7f0f0e2aa57e0b74f7444c5eeb2a00d
“yajashgoplani” <yajash.goplani@gmail.com>
2021-07-07 14:19:32 +0530

    Mini Project

---
+++ wiley_firstproject/src/jpmorgan_banking_withdraw/BankApp.java
@@ -0,0 +1,24 @@
+package jpmorgan_banking_withdraw;
+/*
+ * Login 
+ * *    ---prints balance, acccount number ,branch code
+ * * 	Deposit Amount
+ * * 	Withdraw Amount
+ * * 	Money Transfer
+ * * 	Mini Statement
+ * View Balance
+ * *	---prints balance
+ * OpenAccount
+ * *    ---Ask for necessary details
+ */
+
+
+
+public class BankApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
